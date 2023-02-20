473646d8695d34363909719ffff31be1596e6767
Spencer Hooks <spencerhooks@gmail.com>
2023-02-20 13:27:48 -0800

    working options

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-    let current_url = tabs[0].url
     document.getElementById("my_url").innerHTML = tabs[0].url;
-    
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://mars.hooks.casa:3000/api/shorten", true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('authorization', 'z7WlYEWNIBzGlv7bb77NgSqp.MTY3Njg2NTA1OTg0NA');
-    xhr.send(JSON.stringify({
-        url: current_url
-    }));
+
+    chrome.storage.sync.get(['myServerStored', 'myTokenStored'], function(data) {
+        document.getElementById("my_server").innerHTML = data.myServerStored;   
+ 
+        let serverObj = {};
+        serverObj[url="url"] = tabs[0].url;
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", data.myServerStored+'/api/shorten', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('authorization', data.myTokenStored);
+        xhr.send(JSON.stringify(serverObj));
+    });
+
 });
+
+
+
+
