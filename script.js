bd782bdddc34d3403f51d690e5664ebce9859cae
Spencer Hooks <spencerhooks@gmail.com>
2023-02-20 17:37:58 -0800

    more stuff

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ chrome.tabs.query({active: true, currentWindow: true}, tabs => {
     chrome.storage.sync.get(['myServerStored', 'myTokenStored'], function(data) {
         document.getElementById("my_server").innerHTML = data.myServerStored;   
  
-        let serverObj = {};
-        serverObj[url="url"] = tabs[0].url;
         let xhr = new XMLHttpRequest();
+        xhr.responseType = 'json';
         xhr.open("POST", data.myServerStored+'/api/shorten', true);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.setRequestHeader('authorization', data.myTokenStored);
+
+        xhr.onload = () => {
+            const shortURL = xhr.response;
+            // console.log(shortURL);
+            navigator.clipboard.writeText("testingstuff").then();
+        };
+
+        let serverObj = {};
+        serverObj[url="url"] = tabs[0].url;
+
         xhr.send(JSON.stringify(serverObj));
     });
 
