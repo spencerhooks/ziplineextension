ce5dd0aa98374a5766aa43156a926cb9f34da947
Spencer Hooks <spencerhooks@gmail.com>
2023-02-21 14:28:40 -0800

    hooking things up

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
-chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-    // document.getElementById("my_url").innerHTML = tabs[0].url;
+settingsCog.addEventListener('click', function(){location.href = 'settings.html';});
 
-    chrome.storage.sync.get(['myServerStored', 'myTokenStored'], function(data) {
-        document.getElementById("destination").innerHTML= tabs[0].url;   
- 
-        // let xhr = new XMLHttpRequest();
-        // xhr.responseType = 'json';
-        // xhr.open("POST", data.myServerStored+'/api/shorten', true);
-        // xhr.setRequestHeader('Content-Type', 'application/json');
-        // xhr.setRequestHeader('authorization', data.myTokenStored);
-
-        // xhr.onload = () => {
-        //     const shortURL = xhr.response;
-        //     // console.log(shortURL);
-        //     navigator.clipboard.writeText("testingstuff").then();
-        // };
-
-        // let serverObj = {};
-        // serverObj[url="url"] = tabs[0].url;
-
-        // xhr.send(JSON.stringify(serverObj));
-    });
-
+chrome.storage.sync.get(['myServerStored', 'myTokenStored'], function(data) {
+    // Show the users domain
+    document.getElementById("your-domain").innerHTML = data.myServerStored;
 });
 
-settingsCog.addEventListener('click', function(){location.href = 'settings.html';});
+chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+    document.getElementById("destination").innerHTML= tabs[0].url;   // Write current url to destination field
+    
+    createLink.addEventListener('click', getShortURL);
+
+    function getShortURL(){
+        chrome.storage.sync.get(['myServerStored', 'myTokenStored'], function(data) {
+                       
+            // Create http request headers
+            let xhr = new XMLHttpRequest();
+            xhr.responseType = 'json';
+            xhr.open("POST", data.myServerStored+'/api/shorten', true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.setRequestHeader('authorization', data.myTokenStored);
+
+            // Function to get the short url that is returned
+            xhr.onload = () => {
+                const shortURL = xhr.response;
+                console.log(shortURL);
+                // navigator.clipboard.writeText("testingstuff").then();
+            };
+
+            // Create json payload
+            let serverObj = {};
+            serverObj[url="url"] = tabs[0].url;
+
+            // Send request with payload
+            xhr.send(JSON.stringify(serverObj));
+
+            // setTimeout(function(){window.close(), 1000});
+        });
+    };
+});
+
