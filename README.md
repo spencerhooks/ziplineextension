db5a5da9d6eed83332fc7b965ccd13cfd89fa40a
Spencer Hooks <spencerhooks@gmail.com>
2023-02-21 20:09:09 -0800

    added options page

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ziplineextension
-A Chrome extension to create short URLs using Zipline
+A Chrome extension to create short URLs using Zipline. This extension is being developed and tested (so far) using Brave v1.48.167.
+
+## Screenshots (In Development)
+
+https://h8ks.me/u/yHmiQE.png https://h8ks.me/u/FRpc7u.png
+
--- options.js
+++ options.js
@@ -1,12 +1,18 @@
-serverButton.addEventListener('click', getInputValue);
+chrome.storage.sync.get(['myServerStored', 'myTokenStored'], function(data) {
+    document.getElementById("serverAddress").setAttribute('value', data.myServerStored);
+    document.getElementById("apiToken").setAttribute('value', data.myTokenStored);
+});
+
+saveButton.addEventListener('click', getInputValue);
 
 function getInputValue(){
     
     // Selecting the input element and get its value 
-    var inputServer = document.getElementById("myServer").value;
-    var inputToken = document.getElementById("myToken").value;
+    var inputServer = document.getElementById("serverAddress").value;
+    var inputToken = document.getElementById("apiToken").value;
 
     // Store the value
     chrome.storage.sync.set({ myServerStored: inputServer }); // 
     chrome.storage.sync.set({ myTokenStored: inputToken }); // Is this a secure way to store the token?
 };
+
